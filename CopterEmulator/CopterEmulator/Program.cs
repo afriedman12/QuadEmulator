d8d7bcdcc2ec662a1f2c9a52f233eb0a54b0c493
afriedman12 <alexia@cognosis.com>
2018-04-22 18:23:06 -0700

    Add files via upload

---
+++ CopterEmulator/CopterEmulator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+
+namespace CopterEmulator
+{
+    class Program
+    {
+
+        static readonly Guid CopterGUID = new Guid("633e589e-2036-4fac-8371-d9a9544e9355");
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("--- Copter Emulator ---");
+            System.Console.WriteLine("Listening for commands...");
+            System.Console.WriteLine();
+
+            //Guid serviceClass = BluetoothService.SerialPort;
+            var lsnr = new BluetoothListener(CopterGUID);
+            lsnr.Start();
+
+            BluetoothClient conn = lsnr.AcceptBluetoothClient();
+            Stream peerStream = conn.GetStream();
+
+            byte[] buffer = new byte[256];
+
+            while (!false)
+            {
+                int readSize = peerStream.Read(buffer, 0, buffer.Length);
+                if (readSize > 0)
+                {
+                    String s = System.Text.Encoding.ASCII.GetString(buffer, 0, readSize);
+                    System.Console.Write(s);
+                }
+            }
+            
+        }
+
+        
+    }
+}
